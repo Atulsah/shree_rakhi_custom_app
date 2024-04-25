09ebff1acbe7d3bf9248823a02288c7bc2249e1a
shree <shree@Ubuntu22.04-Erpnext-14.myguest.virtualbox.org>
2024-04-25 11:13:01 +0530

    Added Customer ledger report

---
+++ shree_rakhi_custom_app/shree_rakhi_custom_app/report/custom_customer_ledger/custom_customer_ledger.js
@@ -0,0 +1,41 @@
+// Copyright (c) 2024, Atul Sah and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Custom Customer Ledger"] = {
+	filters: [
+		{
+			fieldname: "company",
+			label: __("Company"),
+			fieldtype: "Link",
+			options: "Company",
+			default: frappe.defaults.get_user_default("Company"),
+		},
+		{
+			fieldname: "customer",
+			label: __("Customer"),
+			fieldtype: "Link",
+			options: "Customer",
+		},
+		{
+			fieldname: "from_date",
+			label: __("From Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.add_months(frappe.datetime.get_today(), -12),
+			width: "80",
+		},
+		{
+			fieldname: "to_date",
+			label: __("To Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.get_today(),
+		},
+		{
+			fieldname: "brand",
+			label: __("Brand"),
+			fieldtype: "Link",
+			options: "Brand",
+		}
+	]
+};
+
--- shree_rakhi_custom_app/shree_rakhi_custom_app/report/custom_customer_ledger/custom_customer_ledger.py
+++ shree_rakhi_custom_app/shree_rakhi_custom_app/report/custom_customer_ledger/custom_customer_ledger.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2024, Atul Sah and contributors
 # For license information, please see license.txt
 
+
 from __future__ import unicode_literals
 import frappe
 from frappe import _
@@ -11,38 +12,38 @@ def execute(filters=None):
 	data=[]
 	data = get_data(filters)
 	columns = get_columns()
-	"""
-		if len(data) == 0:
-			frappe.msgprint('No data!!!')
-			return [], []
-		else:
-			dataframe = pd.DataFrame.from_records(data)
-			data = dataframe.reset_index().to_dict('records')
-	"""
+
 	return columns, data
 
 def get_data(filters):
 	return frappe.db.sql("""
-			select 
-				name, customer, customer_name, place_of_supply, 
-				total_qty, custom_number_of_carton, base_total, 
-				additional_discount_percentage, discount_amount, 
-				rounded_total, custom_bos_transporter 
-			from 
-				`tabSales Invoice`
-		    where 
-				posting_date BETWEEN %(from_date)s and %(to_date)s and 
-				docstatus=1 {itm_conditions} 
-			order by 
-				name Asc""".format(itm_conditions=get_item_conditions(filters)),
-				{'from_date':filters.from_date,'to_date':filters.to_date,
-	 			'customer':filters.customer},as_dict=1) 
+		SELECT 
+			inv.name, inv.customer, inv.customer_name, inv.custom_number_of_carton, 
+			inv.base_total, inv.additional_discount_percentage, inv.discount_amount, 
+			inv.rounded_total, sum(per.allocated_amount) as allocated, min(per.outstanding_amount) as outstanding
+		FROM 
+			`tabSales Invoice` inv 
+		LEFT JOIN 
+			`tabPayment Entry Reference` per
+		ON 
+			per.reference_name = inv.name
+		WHERE 
+			inv.posting_date BETWEEN %(from_date)s and %(to_date)s and 
+			inv.docstatus=1 {itm_conditions} 
+		group by
+			inv.name, inv.customer, inv.customer_name, inv.custom_number_of_carton, 
+			inv.base_total, inv.additional_discount_percentage, inv.discount_amount
+		order by 
+			name Asc""".format(itm_conditions=get_item_conditions(filters)),
+			{'from_date':filters.from_date,'to_date':filters.to_date,
+			'customer':filters.customer},as_dict=1)
+					  
 
 
 def get_item_conditions(filters):
 	conditions = []
 	if filters.get("customer"):
-		conditions.append("customer=%(customer)s")	
+		conditions.append("inv.customer=%(customer)s")	
 
 	return "and {}".format(" and ".join(conditions)) if conditions else ""				
 
@@ -52,30 +53,18 @@ def get_item_conditions(filters):
 #Add columns in report
 def get_columns():
 	columns = [{
+		"fieldname": "customer_name",
+		"label": _("Customer"),  
+		"fieldtype": "Link",
+		"options": "Customer",
+		"width": 150
+	}]
+	columns.append({
 		"fieldname": "name",
 		"label": _("Invoice No"),
 		"fieldtype": "Link",
 		"options": "Sales Invoice",
 		"width": 200
-	}]
-	columns.append({
-		"fieldname": "customer_name",
-		"label": _("Customer"),  
-		"fieldtype": "Link",
-		"options": "Customer",
-		"width": 100
-	})
-	columns.append({
-		"fieldname": "place_of_supply",
-		"label": _("Place"),
-		"fieldtype": "Data",
-		"width": 100
-	})
-	columns.append({
-		"fieldname": "total_qty",
-		"label": _("Quantity"),
-		"fieldtype": "Data",
-		"width": 100
 	})
 	columns.append({
 		"fieldname": "custom_number_of_carton",
@@ -108,10 +97,19 @@ def get_columns():
 		"width": 100
 	})
 	columns.append({
-		"fieldname": "transport",
-		"label": _("Transport"),
+		"fieldname": "allocated",
+		"label": _("Amount Paid"),
 		"fieldtype": "Data",
-		"width": 150
+		"width": 100
 	})
+	columns.append({
+		"fieldname": "outstanding",
+		"label": _(" Outstanding Amount"),
+		"fieldtype": "Data",
+		"width": 100
+	})
+
 		
 	return columns	
+
+
